8b7ed05074522a1ddc7824992360c74e170df20b
U10416011 <u10416011@go.utaipei.edu.tw>
2016-03-16 19:35:28 +0800

    ver1.00
    Bean machine pane without any action

---
+++ BeanMachinePane.java
@@ -0,0 +1,349 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
+
+
+public class BeanMachinePane  extends Application {
+
+	public void start(Stage primaryStage) {
+		//create a pane
+		Pane pane = new Pane();
+		
+		//create 28 circle as dots
+		Circle Dot1 = new Circle();
+		Dot1.setLayoutX(275);
+		Dot1.setLayoutY(100);
+		Dot1.setRadius(5);
+		Dot1.setStroke(Color.BLACK);
+		Dot1.setFill(Color.BLACK);
+		pane.getChildren().add(Dot1);
+		
+		Circle Dot2 = new Circle();
+		Dot2.setLayoutX(250);
+		Dot2.setLayoutY(130);
+		Dot2.setRadius(5);
+		Dot2.setStroke(Color.BLACK);
+		Dot2.setFill(Color.BLACK);
+		pane.getChildren().add(Dot2);
+		
+		Circle Dot3 = new Circle();
+		Dot3.setLayoutX(300);
+		Dot3.setLayoutY(130);
+		Dot3.setRadius(5);
+		Dot3.setStroke(Color.BLACK);
+		Dot3.setFill(Color.BLACK);
+		pane.getChildren().add(Dot3);
+		
+		Circle Dot4 = new Circle();
+		Dot4.setLayoutX(275);
+		Dot4.setLayoutY(160);
+		Dot4.setRadius(5);
+		Dot4.setStroke(Color.BLACK);
+		Dot4.setFill(Color.BLACK);
+		pane.getChildren().add(Dot4);
+		
+		Circle Dot5 = new Circle();
+		Dot5.setLayoutX(325);
+		Dot5.setLayoutY(160);
+		Dot5.setRadius(5);
+		Dot5.setStroke(Color.BLACK);
+		Dot5.setFill(Color.BLACK);
+		pane.getChildren().add(Dot5);
+		
+		Circle Dot6 = new Circle();
+		Dot6.setLayoutX(225);
+		Dot6.setLayoutY(160);
+		Dot6.setRadius(5);
+		Dot6.setStroke(Color.BLACK);
+		Dot6.setFill(Color.BLACK);
+		pane.getChildren().add(Dot6);
+		
+		Circle Dot7 = new Circle();
+		Dot7.setLayoutX(200);
+		Dot7.setLayoutY(190);
+		Dot7.setRadius(5);
+		Dot7.setStroke(Color.BLACK);
+		Dot7.setFill(Color.BLACK);
+		pane.getChildren().add(Dot7);
+		
+		Circle Dot8 = new Circle();
+		Dot8.setLayoutX(350);
+		Dot8.setLayoutY(190);
+		Dot8.setRadius(5);
+		Dot8.setStroke(Color.BLACK);
+		Dot8.setFill(Color.BLACK);
+		pane.getChildren().add(Dot8);
+		
+		Circle Dot9 = new Circle();
+		Dot9.setLayoutX(250);
+		Dot9.setLayoutY(190);
+		Dot9.setRadius(5);
+		Dot9.setStroke(Color.BLACK);
+		Dot9.setFill(Color.BLACK);
+		pane.getChildren().add(Dot9);
+		
+		Circle Dot10 = new Circle();
+		Dot10.setLayoutX(300);
+		Dot10.setLayoutY(190);
+		Dot10.setRadius(5);
+		Dot10.setStroke(Color.BLACK);
+		Dot10.setFill(Color.BLACK);
+		pane.getChildren().add(Dot10);
+		
+		Circle Dot11 = new Circle();
+		Dot11.setLayoutX(375);
+		Dot11.setLayoutY(220);
+		Dot11.setRadius(5);
+		Dot11.setStroke(Color.BLACK);
+		Dot11.setFill(Color.BLACK);
+		pane.getChildren().add(Dot11);
+		
+		Circle Dot12 = new Circle();
+		Dot12.setLayoutX(175);
+		Dot12.setLayoutY(220);
+		Dot12.setRadius(5);
+		Dot12.setStroke(Color.BLACK);
+		Dot12.setFill(Color.BLACK);
+		pane.getChildren().add(Dot12);
+		
+		Circle Dot13 = new Circle();
+		Dot13.setLayoutX(225);
+		Dot13.setLayoutY(220);
+		Dot13.setRadius(5);
+		Dot13.setStroke(Color.BLACK);
+		Dot13.setFill(Color.BLACK);
+		pane.getChildren().add(Dot13);
+		
+		Circle Dot14 = new Circle();
+		Dot14.setLayoutX(275);
+		Dot14.setLayoutY(220);
+		Dot14.setRadius(5);
+		Dot14.setStroke(Color.BLACK);
+		Dot14.setFill(Color.BLACK);
+		pane.getChildren().add(Dot14);
+		
+		Circle Dot15 = new Circle();
+		Dot15.setLayoutX(325);
+		Dot15.setLayoutY(220);
+		Dot15.setRadius(5);
+		Dot15.setStroke(Color.BLACK);
+		Dot15.setFill(Color.BLACK);
+		pane.getChildren().add(Dot15);
+		
+		Circle Dot16 = new Circle();
+		Dot16.setLayoutX(400);
+		Dot16.setLayoutY(250);
+		Dot16.setRadius(5);
+		Dot16.setStroke(Color.BLACK);
+		Dot16.setFill(Color.BLACK);
+		pane.getChildren().add(Dot16);
+		
+		Circle Dot17 = new Circle();
+		Dot17.setLayoutX(150);
+		Dot17.setLayoutY(250);
+		Dot17.setRadius(5);
+		Dot17.setStroke(Color.BLACK);
+		Dot17.setFill(Color.BLACK);
+		pane.getChildren().add(Dot17);
+		
+		Circle Dot18 = new Circle();
+		Dot18.setLayoutX(200);
+		Dot18.setLayoutY(250);
+		Dot18.setRadius(5);
+		Dot18.setStroke(Color.BLACK);
+		Dot18.setFill(Color.BLACK);
+		pane.getChildren().add(Dot18);
+		
+		Circle Dot19 = new Circle();
+		Dot19.setLayoutX(250);
+		Dot19.setLayoutY(250);
+		Dot19.setRadius(5);
+		Dot19.setStroke(Color.BLACK);
+		Dot19.setFill(Color.BLACK);
+		pane.getChildren().add(Dot19);
+		
+		Circle Dot20 = new Circle();
+		Dot20.setLayoutX(300);
+		Dot20.setLayoutY(250);
+		Dot20.setRadius(5);
+		Dot20.setStroke(Color.BLACK);
+		Dot20.setFill(Color.BLACK);
+		pane.getChildren().add(Dot20);
+		
+		Circle Dot21 = new Circle();
+		Dot21.setLayoutX(350);
+		Dot21.setLayoutY(250);
+		Dot21.setRadius(5);
+		Dot21.setStroke(Color.BLACK);
+		Dot21.setFill(Color.BLACK);
+		pane.getChildren().add(Dot21);
+		
+		Circle Dot22 = new Circle();
+		Dot22.setLayoutX(425);
+		Dot22.setLayoutY(280);
+		Dot22.setRadius(5);
+		Dot22.setStroke(Color.BLACK);
+		Dot22.setFill(Color.BLACK);
+		pane.getChildren().add(Dot22);
+		
+		Circle Dot23 = new Circle();
+		Dot23.setLayoutX(125);
+		Dot23.setLayoutY(280);
+		Dot23.setRadius(5);
+		Dot23.setStroke(Color.BLACK);
+		Dot23.setFill(Color.BLACK);
+		pane.getChildren().add(Dot23);
+		
+		Circle Dot24 = new Circle();
+		Dot24.setLayoutX(175);
+		Dot24.setLayoutY(280);
+		Dot24.setRadius(5);
+		Dot24.setStroke(Color.BLACK);
+		Dot24.setFill(Color.BLACK);
+		pane.getChildren().add(Dot24);
+		
+		Circle Dot25 = new Circle();
+		Dot25.setLayoutX(225);
+		Dot25.setLayoutY(280);
+		Dot25.setRadius(5);
+		Dot25.setStroke(Color.BLACK);
+		Dot25.setFill(Color.BLACK);
+		pane.getChildren().add(Dot25);
+		
+		Circle Dot26 = new Circle();
+		Dot26.setLayoutX(275);
+		Dot26.setLayoutY(280);
+		Dot26.setRadius(5);
+		Dot26.setStroke(Color.BLACK);
+		Dot26.setFill(Color.BLACK);
+		pane.getChildren().add(Dot26);
+		
+		Circle Dot27 = new Circle();
+		Dot27.setLayoutX(325);
+		Dot27.setLayoutY(280);
+		Dot27.setRadius(5);
+		Dot27.setStroke(Color.BLACK);
+		Dot27.setFill(Color.BLACK);
+		pane.getChildren().add(Dot27);
+		
+		Circle Dot28 = new Circle();
+		Dot28.setLayoutX(375);
+		Dot28.setLayoutY(280);
+		Dot28.setRadius(5);
+		Dot28.setStroke(Color.BLACK);
+		Dot28.setFill(Color.BLACK);
+		pane.getChildren().add(Dot28);
+		
+		
+		//create line as side
+		Line Line1 = new Line();
+		Line1.setStartX(300);
+		Line1.setStartY(65);
+		Line1.setEndX(475);
+		Line1.setEndY(280);
+		pane.getChildren().addAll(Line1);
+		
+		Line Line2 = new Line();
+		Line2.setStartX(475);
+		Line2.setStartY(350);
+		Line2.setEndX(75);
+		Line2.setEndY(350);
+		pane.getChildren().addAll(Line2);
+		
+		Line Line3 = new Line();
+		Line3.setStartX(250);
+		Line3.setStartY(65);
+		Line3.setEndX(75);
+		Line3.setEndY(280);
+		pane.getChildren().addAll(Line3);
+		
+		Line Line4 = new Line();
+		Line4.setStartX(475);
+		Line4.setStartY(280);
+		Line4.setEndX(475);
+		Line4.setEndY(350);
+		pane.getChildren().addAll(Line4);
+		
+		Line Line5 = new Line();
+		Line5.setStartX(75);
+		Line5.setStartY(350);
+		Line5.setEndX(75);
+		Line5.setEndY(280);
+		pane.getChildren().addAll(Line5);
+		
+		Line Line6 = new Line();
+		Line6.setStartX(300);
+		Line6.setStartY(65);
+		Line6.setEndX(300);
+		Line6.setEndY(40);
+		pane.getChildren().addAll(Line6);
+		
+		Line Line7 = new Line();
+		Line7.setStartX(250);
+		Line7.setStartY(65);
+		Line7.setEndX(250);
+		Line7.setEndY(40);
+		pane.getChildren().addAll(Line7);
+		
+		Line Line8 = new Line();
+		Line8.setStartX(125);
+		Line8.setStartY(280);
+		Line8.setEndX(125);
+		Line8.setEndY(350);
+		pane.getChildren().addAll(Line8);
+		
+		Line Line9 = new Line();
+		Line9.setStartX(175);
+		Line9.setStartY(280);
+		Line9.setEndX(175);
+		Line9.setEndY(350);
+		pane.getChildren().addAll(Line9);
+		
+		Line Line10 = new Line();
+		Line10.setStartX(225);
+		Line10.setStartY(280);
+		Line10.setEndX(225);
+		Line10.setEndY(350);
+		pane.getChildren().addAll(Line10);
+		
+		Line Line11 = new Line();
+		Line11.setStartX(275);
+		Line11.setStartY(280);
+		Line11.setEndX(275);
+		Line11.setEndY(350);
+		pane.getChildren().addAll(Line11);
+		
+		Line Line12 = new Line();
+		Line12.setStartX(325);
+		Line12.setStartY(280);
+		Line12.setEndX(325);
+		Line12.setEndY(350);
+		pane.getChildren().addAll(Line12);
+		
+		Line Line13 = new Line();
+		Line13.setStartX(425);
+		Line13.setStartY(280);
+		Line13.setEndX(425);
+		Line13.setEndY(350);
+		pane.getChildren().addAll(Line13);
+		
+		Line Line14 = new Line();
+		Line14.setStartX(375);
+		Line14.setStartY(280);
+		Line14.setEndX(375);
+		Line14.setEndY(350);
+		pane.getChildren().addAll(Line14);
+		
+		//create a scene
+		Scene scene = new Scene(pane,600,500);
+		primaryStage.setTitle("BeanMachinePane");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+}
